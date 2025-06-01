1dcb75b6f5209c84e303d49a86efc516c73cb63a
duongquangvinh <duongquangvinh97@gmail.com>
2025-06-01 11:23:29 +1000

    update code structure

--- src/config/env_setup.py
+++
@@ -1,7 +0,0 @@
-import getpass
-import os
-
-
-def _set_env(var: str):
-    if not os.environ.get(var):
-        os.environ[var] = getpass.getpass(f"{var}: ")
--- src/tools/search_tool.py
+++
@@ -1,4 +0,0 @@
-from langchain_community.tools.tavily_search import TaviSearchResults
-
-
-tool = TaviSearchResults(max_results=2)
